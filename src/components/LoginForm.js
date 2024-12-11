edc4bef6e63bf288fba33705a3b9ffe974bcba96
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-12 01:14:13 +0530

    this is my signup commit

---
+++ src/components/LoginForm.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {AiOutlineEye,AiOutlineEyeInvisible} from "react-icons/ai";
+import { Link } from 'react-router-dom';
+
+const LoginForm = () => {
+    const [formData,setFormData]=useState({
+        email:"",
+        password:""
+    })
+    const [showPassword,setShowPassword]=useState(false);
+    function changeHandler(event){
+        setFormData((prevData)=>({...prevData,
+            [event.target.name]:event.target.value
+        }))
+
+    }
+  return (
+    <form>
+        <label>
+            <p>Email address:<sup>*</sup></p>
+            <input
+            required
+            type="email"
+            value={formData.email}
+            onChange={changeHandler}
+            placeholder="enter email id"
+            name="email"/>
+        </label>
+        <label>
+            <p>Password:<sup>*</sup></p>
+            <input
+            required
+            type={showPassword?("text"):("password")}
+            value={formData.password}
+            onChange={changeHandler}
+            placeholder="enter password"
+            name="password"/>
+            <span onClick={()=>{setShowPassword((prev)=>!prev)}}>
+                {showPassword?(<AiOutlineEyeInvisible/>):(<AiOutlineEye/>)}
+            </span>
+            <Link to="#"><p>forget password</p></Link>
+        </label>
+        <button>
+            Sign in
+        </button>
+    </form>
+  )
+}
+
+export default LoginForm
