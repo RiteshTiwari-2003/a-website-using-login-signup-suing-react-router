055c7a9df7fec476096f973654b57da32f67c152
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-13 01:18:08 +0530

    this is my sign up twenty

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = (props) => {
     <div className="flex justify-between items-center w-11/12 max-w-[1160px] py-4 mx-auto">
       <Link to="/"><img src={logo} alt="logo" width={160} height={32} loading="lazy"/></Link> 
       <nav>
-        <ul className="text-white flex gap-x-6">
+        <ul className="text-richblack-100 flex gap-x-6">
             <li>
                 <Link to="/">Home</Link>
             </li>
@@ -27,19 +27,19 @@ const Navbar = (props) => {
       </nav>
       <div className="flex items-center gap-x-4">
         {!isLoggedIn &&
-            <Link to="/login"><button className="bg-richblack-800 text-white py-[8px] px-[12px] rounded-[8px] border border-richblack-700">Login</button></Link>
+            <Link to="/login"><button className="bg-richblack-800 text-richblack-100 py-[8px] px-[12px] rounded-[8px] border border-richblack-700">Login</button></Link>
         }
         {!isLoggedIn &&
-            <Link to="/signup"><button className="bg-richblack-800 text-white py-[8px] px-[12px] rounded-[8px] border border-richblack-700">Sign up</button></Link>
+            <Link to="/signup"><button className="bg-richblack-800 text-richblack-100 py-[8px] px-[12px] rounded-[8px] border border-richblack-700">Sign up</button></Link>
         }
         { isLoggedIn &&
             <Link to="/"><button onClick={()=>{
               setIsLoggedIn(false);
               toast.success("Logged Out");
-            }} className="bg-richblack-800 text-white py-[8px] px-[12px] rounded-[8px] border border-richblack-700">Log out</button></Link>
+            }} className="bg-richblack-800 text-richblack-100 py-[8px] px-[12px] rounded-[8px] border border-richblack-700">Log out</button></Link>
         }
         { isLoggedIn &&
-            <Link to="/dashboard"><button className="bg-richblack-800 text-white py-[8px] px-[12px] rounded-[8px] border border-richblack-700">Dashboard</button></Link>
+            <Link to="/dashboard"><button className="bg-richblack-800 text-richblack-100 py-[8px] px-[12px] rounded-[8px] border border-richblack-700">Dashboard</button></Link>
         }
       </div>
     </div>
