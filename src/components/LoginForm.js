60bbb0afa0b79dbab2c86ab9357b2c41f86738a5
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-12 17:03:24 +0530

    this is my sign up 10 commit

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom';
 import {useState} from "react";
 import { toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import { useNavigate } from 'react-router-dom';
 
 const LoginForm = ({setIsLoggedIn}) => {
+    const navigate=useNavigate();
     const [formData,setFormData]=useState({
         email:"",
         password:""
