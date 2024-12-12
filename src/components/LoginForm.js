b764b8328e6f5ef61cc28e68dd3f57cf2e90acb1
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-13 02:01:33 +0530

    this is my sign up twenty eigth commit

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -27,26 +27,28 @@ const LoginForm = ({setIsLoggedIn}) => {
 
     }
   return (
-    <form onSubmit={submitHandler}>
-        <label>
-            <p>Email address:<sup>*</sup></p>
+    <form onSubmit={submitHandler} className='flex flex-col w-full gap-y-4 mt-6 '>
+        <label className="w-full ">
+            <p className="text-[0.875rem] text-richblack-5 mb-1 leading-[1.375rem] ">Email address:<sup className="text-pink-200">*</sup></p>
             <input
             required
             type="email"
             value={formData.email}
             onChange={changeHandler}
             placeholder="enter email id"
-            name="email"/>
+            name="email"
+            classname="bg-richblack-800 rounded-[0.5rem] text-richblack-5 w-full p-[12px]"/>
         </label>
-        <label>
-            <p>Password:<sup>*</sup></p>
+        <label className="w-full ">
+            <p className="text-[0.875rem] text-richblack-5 mb-1 leading-[1.375rem] ">Password:<sup>*</sup></p>
             <input
             required
             type={showPassword?("text"):("password")}
             value={formData.password}
             onChange={changeHandler}
             placeholder="enter password"
-            name="password"/>
+            name="password"
+            classname="bg-richblack-800 rounded-[0.5rem] text-richblack-5 w-full p-[12px]"/>
             <span onClick={()=>{setShowPassword((prev)=>!prev)}}>
                 {showPassword?(<AiOutlineEyeInvisible/>):(<AiOutlineEye/>)}
             </span>
